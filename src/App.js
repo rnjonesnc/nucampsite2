0ffe0272f3817a4ba7ea78cfa286c5b2907bcd02
rnjonesnc <ravenjones@Ravens-MBP-2.lan>
2022-02-28 23:20:40 -0500

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//Got this from the written instructions React - Week 2 - 7. Exercise: Inital Configuration
+class App 
+  extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Navbar dark color="primary">
+              <div className="container">
+                  <NavbarBrand href="/">NuCamp</NavbarBrand>
+              </div>
+              </Navbar>
+          </div>
+      );
+  }
 }
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
